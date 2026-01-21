e82ed3c9c4e3a4397341459afdf9ca16e647e101
Durgeshkr01 <durgeshraj08521@gmail.com>
2026-01-22 00:16:32 +0530

    Updated to v57 - Added notification filters, dues reminders, auto-sync, and performance optimizations

---
+++ sw.js
@@ -0,0 +1,64 @@
+const CACHE_NAME = 'sg-fitness-evolution-v57';
+const urlsToCache = [
+    './',
+    './index.html',
+    './manifest.json',
+    './logo.png',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+    'https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js'
+];
+
+// Install service worker
+self.addEventListener('install', event => {
+    console.log('New version installing...');
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+    // Force waiting service worker to become active
+    self.skipWaiting();
+});
+
+// Fetch from cache or network
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            }
+        )
+    );
+});
+
+// Update cache and notify clients
+self.addEventListener('activate', event => {
+    console.log('New version activated!');
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        console.log('Deleting old cache:', cacheName);
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        }).then(() => {
+            // Take control of all clients immediately
+            return self.clients.claim();
+        }).then(() => {
+            // Notify all clients about the update
+            return self.clients.matchAll().then(clients => {
+                clients.forEach(client => {
+                    client.postMessage({ type: 'UPDATE_AVAILABLE' });
+                });
+            });
+        })
+    );
+});
